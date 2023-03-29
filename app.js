635b1d94e7f2f83431cb3724a02f163edd89ace8
charithtagore <122634098+charithtagore@users.noreply.github.com>
2023-03-29 11:14:20 -0500

    Changes Done

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var mydataRouter = require('./routes/mydata');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -19,6 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/mydata', mydataRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
